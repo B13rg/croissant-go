d8605fdfc0df91dea2933282cada66d0ca5e948d
b13rg <13488329+B13rg@users.noreply.github.com>
2025-08-06 23:49:26 -0700

    add util and error type

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,2 +1,15 @@
-// Place external shared types here.
+// Place internal types here.
 package types
+
+import "fmt"
+
+type CroissantError struct {
+	// Message to show the user.
+	Message string
+	// Value to include with message
+	Value any
+}
+
+func (e CroissantError) Error() string {
+	return fmt.Sprintf("%s: %v", e.Message, e.Value)
+}
---
+++ pkg/util/error.go
@@ -0,0 +1,12 @@
+package util
+
+import "github.com/b13rg/croissant-go/pkg/types"
+
+// If err != nil, wraps it in a Kr8Error with the message.
+func ErrorIfCheck(message string, err error) error {
+	if err != nil {
+		return types.CroissantError{Message: message, Value: err}
+	}
+
+	return nil
+}
